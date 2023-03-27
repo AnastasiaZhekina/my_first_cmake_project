8c102c0bec1b74e7486173c3cd4a971912ff3461
AnastasiaZhekina <yfcnzjkzdbnz09@gmail.com>
2023-03-28 03:33:50 +0500

    first

---
+++ my_first_cmake_program.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream> 
+#include <string> 
+#include <windows.h> 
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	string name;
+	cout << "Введите имя: ";
+	cin >> name;
+
+	cout << "Здравствуйте, " << name << "!";
+}
---
+++ my_first_cmake_program.h
@@ -0,0 +1,9 @@
+﻿#pragma once 
+
+#include <iostream> 
+#include <iostream>  
+#include <string>  
+#include <windows.h>  
+using namespace std;
+
+int main();
